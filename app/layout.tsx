f0ca91132b53e9d051d7323f513545dbc625f9d6
Pedro Ansio <pedroansio@MacBook-Air-de-Pedro.local>
2025-07-23 08:51:18 +0200

    velocidad de página

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Meerkat Lending – A Smarter Way to Borrow',
